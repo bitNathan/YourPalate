d7d88316fe9724def2605618618e29ce5beef70a
bitNathan <53419123+bitNathan@users.noreply.github.com>
2024-09-25 20:57:45 -0400

    fixed actions logging bug

--- src/web_design/sample_site/manage.py
+++ src/web_design/sample_site/manage.py
@@ -8,7 +8,13 @@ import logging
 def main():
     # Set up logging
     path = os.path.dirname(os.path.abspath(__file__))
-    logging.basicConfig(filename=path+'../../../../logs/manage.log', level=logging.INFO)
+    # create logs directory if it doesn't exist (needed for github actions)
+    log_file=path+'../../../../logs/manage.log'
+    if not os.path.exists(log_file):
+        os.makedirs(os.path.dirname(log_file), exist_ok=True)
+        with open(log_file, 'w'):
+            log_file.append('This log file was created automatically.\n')
+    logging.basicConfig(filename=log_file, level=logging.INFO)
     logger.info('Starting manage.py')
     
     """Run administrative tasks."""
