dc30a7aa68fd4ec56cb9283be311d6ae1f3939e1
bitNathan <53419123+bitNathan@users.noreply.github.com>
2024-09-25 20:39:11 -0400

    updated django test cases

--- tests/test_django.py
+++ tests/test_django.py
@@ -29,7 +29,7 @@ class TestDjango(tc):
         client = Client()
         response = client.get("http://127.0.0.1:8000/initialApp/loading/")
         assert response.status_code == 200, "Failed to connect to Django server"
-    def false_test(self):
+    def test_fake_url(self):
         client = Client()
         response = client.get("http://127.0.0.1:8000/fakeUrl")
         assert response.status_code == 404, "Did not return 404 for fake URL"
