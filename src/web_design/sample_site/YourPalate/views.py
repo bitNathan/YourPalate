fadd0c67b86380b37d77936e8a4343d981bca061
bitNathan <53419123+bitNathan@users.noreply.github.com>
2024-10-21 12:39:53 -0400

    added explanation comments

--- src/web_design/sample_site/YourPalate/views.py
+++ src/web_design/sample_site/YourPalate/views.py
@@ -32,6 +32,7 @@ def restrictions(request):
 
 
 def results(request):
+    # running the recommender
     output = recommender_module.run().name.values
     return render(request, 'results.html', {'output': output})
 
