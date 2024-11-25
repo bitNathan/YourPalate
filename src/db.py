2ec1c5ce92b16d86491054da9cf2cfe7ece0a48d
kumar488@purdue.edu <ikumar@trustrelate.com>
2024-11-25 14:36:57 -0500

    syntax fixes

--- src/db.py
+++ src/db.py
@@ -258,7 +258,7 @@ def get_recipes_as_dataframe(recipe_ids):
     return df
 
 
-def get__table_as_dataframe(table_name):
+def get_table_as_dataframe(table_name):
     conn = create_connection()
     try:
         query = f"SELECT * FROM {table_name};"
